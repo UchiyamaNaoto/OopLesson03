c52dfbeb02226697f02ac1556b6c1a0ee05fc6b7
NaotoUchiyama <n-uchiyama@ojs.ac.jp>
2020-10-01 10:37:48 +0900

    P117 リスト4.28

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -7,19 +7,25 @@ using System.Threading.Tasks;
 namespace Chapter4 {
     class Program {
         static void Main(string[] args) {
-            string code = "12345";
-
-            var message = GetMessage(code) ?? DefaultMessage();
-            Console.WriteLine(message);
+            Console.WriteLine(GetProduct());
         }
 
-        //スタブ
-        private static object DefaultMessage() {
-            return "DefaultMessage";
-        }
-        //スタブ
-        private static object GetMessage(string code) {
-            return 123;
-        }
+        private static string GetProduct() {
+            Sale sale = new Sale {
+                ShopName = "pet store",
+                Amount = 100000,
+                Product = "food"
+            };
+            sale = null;
+            return sale?.Product;
+        }        
+    }
+
+    class Sale {
+        // 店舗名   
+        public string ShopName { get; set; }
+        // 売上高   
+        public int Amount { get; set; }
+        public string Product { get; set; }
     }
 }
