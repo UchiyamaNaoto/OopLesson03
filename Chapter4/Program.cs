7b9d504e746c5417d7796510521c2a6ce7b72217
NaotoUchiyama <n-uchiyama@ojs.ac.jp>
2020-10-02 14:01:58 +0900

    P133 問題4.1完成

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -7,25 +7,9 @@ using System.Threading.Tasks;
 namespace Chapter4 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine(GetProduct());
+
+
         }
-
-        private static string GetProduct() {
-            Sale sale = new Sale {
-                ShopName = "pet store",
-                Amount = 100000,
-                Product = "food"
-            };
-            sale = null;
-            return sale?.Product;
-        }        
-    }
-
-    class Sale {
-        // 店舗名   
-        public string ShopName { get; set; }
-        // 売上高   
-        public int Amount { get; set; }
-        public string Product { get; set; }
+      
     }
 }
