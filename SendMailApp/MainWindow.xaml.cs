61c147f7420c28b36a5a7cd4f2d31b8722239c02
NaotoUchiyama <n-uchiyama@ojs.ac.jp>
2020-11-02 13:56:05 +0900

    UI完成

--- SendMailApp/MainWindow.xaml.cs
+++ SendMailApp/MainWindow.xaml.cs
@@ -21,5 +21,15 @@ namespace SendMailApp {
         public MainWindow() {
             InitializeComponent();
         }
+
+        //メール送信処理
+        private void btOk_Click(object sender, RoutedEventArgs e) {
+
+
+        }
+        //送信キャンセル処理
+        private void btCancel_Click(object sender, RoutedEventArgs e) {
+
+        }
     }
 }
