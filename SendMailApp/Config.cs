3bf908e41027f34102bda9891128fc8207739dfe
NaotoUchiyama <n-uchiyama@ojs.ac.jp>
2020-11-06 16:01:56 +0900

    途中経過_1106

--- SendMailApp/Config.cs
+++ SendMailApp/Config.cs
@@ -5,12 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SendMailApp {
-    class Config {
+    public class Config {
+        //単一のインスタンス
+        private static Config instance;
+        
+        //インスタンスの取得
+        public static Config GetInstance() {
+            if (instance == null) {
+                instance = new Config();
+            }
+            return instance;
+        }
+
         public string Smtp { get; set; }    //SMTPサーバー
         public string MailAddress { get; set; } //自メールアドレス（送信元）
         public string PassWord { get; set; }    //パスワード
         public int Port { get; set; }   //ポート番号   
         public bool Ssl { get; set; }   //SSL設定
+
+        //コンストラクタ(外部からnewできないようにする)
+        private Config() {}
 
         //初期設定用
         public void DefaultSet() {
@@ -19,6 +33,30 @@ namespace SendMailApp {
             PassWord = "ojsInfosys2020";
             Port = 587;
             Ssl = true;
+        }
+
+        //初期値取得用
+        public Config getDefaultStatus() {
+            Config obj = new Config {
+                Smtp = "smtp.gmail.com",
+                MailAddress = "ojsinfosys01@gmail.com",
+                PassWord = "ojsInfosys2020",
+                Port = 587,
+                Ssl = true,
+            };
+            return obj;
+        }
+
+        //設定データ更新
+        //public bool UpdateStatus(Config cf) {
+        public bool UpdateStatus(string smtp,string mailAddress, 
+                                            string passWord,int port,bool ssl) {
+            this.Smtp = smtp;
+            this.MailAddress = mailAddress;
+            this.PassWord = passWord;
+            this.Port = port;
+            this.Ssl = ssl;
+            return true;
         }
     }
 }
